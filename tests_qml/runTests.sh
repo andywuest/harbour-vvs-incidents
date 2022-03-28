67595d2a2398d5511ee38da5c075210d3e7e05cc
andywuest <andreas.wuest.freelancer@gmail.com>
2022-03-28 21:45:37 +0200

    fixed execution of qml tests in github action;

--- tests_qml/runTests.sh
+++ tests_qml/runTests.sh
@@ -2,5 +2,5 @@
 
 find . -name "*.xml" -exec rm  {} \;
 
-env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 qmltestrunner -o qmlresults.xml,xml
+env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 qmltestrunner -platform offscreen -o qmlresults.tap,tap
 
